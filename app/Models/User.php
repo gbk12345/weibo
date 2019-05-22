afb781ad708b324cf3bf7e46237faf1c8ee2f539
e <471461204@qq.com>
2019-05-22 16:08:46 +0800

    数据迁移，用户模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    /**
+     * @var array
+     * 允许赋值的字段
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+    /**
+     * @var array
+     * 要隐藏的字段
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
