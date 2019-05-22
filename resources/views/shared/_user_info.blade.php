0e38cc372146bbd1910272f8f8d7a57a2f94b1ec
e <471461204@qq.com>
2019-05-22 16:40:46 +0800

    完成用户注册功能

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('users.show', $user->id) }}">
     <img width="80px;" src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar" style=" border-radius: 50%;"/>
 </a>
-<h1>{{ $user->name }}</h1>c
+<h1>{{ $user->name }}</h1>
